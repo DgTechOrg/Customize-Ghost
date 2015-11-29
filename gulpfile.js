ba937c3502c0b063f30752630aae15fd41df8355
DavidZhang <strucoder@DavidZhangs-MacBook-Pro.local>
2015-11-29 15:11:15 +0800

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,24 @@
 
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
+const merge = require('merge-stream');
 
+gulp.task('min-ghostjs', function() {
+  return gulp.src('./core/built/assets/ghost.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./core/built/assets/ghostmin.js'))
+});
+
+gulp.task('min-vendorjs', function() {
+  return gulp.src('./core/built/assets/vendor.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./core/built/assets/vendormin.js'))
+});
+
+gulp.task('copyjs', function() {
+  return gulp.src('./front/js/*.js')
+    .pipe(gulp.dest('./core/built/assets'))
+});
+
+gulp.task('default', ['copyjs']);
+gulp.task('pro', ['min-ghostjs', 'min-vendorjs']);
